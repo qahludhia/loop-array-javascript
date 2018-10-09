e6e5f20f8e13241531ddbcfb1841c06d817b5fe2
qahludhia <qahludhia@gmail.com>
2018-10-09 11:34:35 +0700

    loop javascript

---
+++ loop-array.js
@@ -0,0 +1,8 @@
+function dataHandling(input){
+	console.log('Nomor ID: ' + input[0][0]);
+	console.log('Nama Lengkap: ' + input[0][1]);
+	console.log('TTL: ' + input[0][2] + " " + input [0][3]);
+	console.log('Hobi: ' + input[0][4]);
+}
+dataHandling(input)
+
