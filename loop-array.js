31a63a5c5bdc1afcc59018b1945563f9fa01d9f4
qahludhia <qahludhia@gmail.com>
2018-10-09 12:28:23 +0700

    learn loop

--- loop-array.js
+++ loop-array.js
@@ -1,8 +1,20 @@
-function dataHandling(input){
-	console.log('Nomor ID: ' + input[0][0]);
-	console.log('Nama Lengkap: ' + input[0][1]);
-	console.log('TTL: ' + input[0][2] + " " + input [0][3]);
-	console.log('Hobi: ' + input[0][4]);
+function dataHandling(){
+
+	var input = [
+		["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+		["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+		["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+		["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+	]
+
+	for (let i = 4; i < input.length; i++) {
+ 	
+
+	console.log('Nomor ID: ' + input[i][0]);
+	console.log('Nama Lengkap: ' + input[i][1]);
+	console.log('TTL: ' + input[i][2] + " " + input [i][3]);
+	console.log('Hobi: ' + input[i][4]);
+		}
 }
-dataHandling(input)
+dataHandling()
 
